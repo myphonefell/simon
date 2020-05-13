33574a7e2b9b824de820d04681b0761af7ade6a7
myphonefell <58866386+myphonefell@users.noreply.github.com>
2020-05-13 15:42:50 -0400

    Update View.java

--- src/simon/views/View.java
+++ src/simon/views/View.java
@@ -9,46 +9,41 @@ import simon.controllers.ColourSquare;
 
 public class View extends JPanel{
 	private JLabel title;
-	ColourSquare[] boxes=new ColourSquare[4];
 	JPanel a,b;
-	public int[] sequenceInput = {5,5,5,5};
-	
+	private SequenceListener in;
 	public View() {
 		super();
 		this.setBackground(Color.BLACK);
-		init();
 	}
-	private void init() {
+	
+	
+
+	public void init() {	
 		a=new JPanel(new GridLayout(2,1));
-		b=new JPanel(new GridLayout(2,2));
-		
 		title=new JLabel("Simon Says");
 		title.setForeground(Color.WHITE);
 	    title.setFont(new Font("Jokerman",Font.ITALIC,40));
 	    
-		boxes[0]=new ColourSquare(Color.RED,0,this);
-		boxes[1]=new ColourSquare(Color.YELLOW,1,this);
-		boxes[2]=new ColourSquare(Color.BLUE,2,this);
-		boxes[3]=new ColourSquare(Color.GREEN,3,this);
 		
-		a.setBackground(Color.BLACK);
 		a.add(title);
-		
-		b.setBackground(Color.BLACK);
-		b.add(boxes[0]);
-		b.add(boxes[1]);
-		b.add(boxes[2]);
-		b.add(boxes[3]);
-		
-		a.add(b);
-		
+		a.setBackground(Color.BLACK);
 		this.add(a);
+				
 	}
 	
-	private void endScreen() {
-		title.setText("Game Over");
-		a.remove(b);
+	public void startScreen() {
+		title.setText("Simon Says");
+		repaint();
+	}
+	
+	public void endScreen() {
+		title.setText("Incorrect Sequence ");
+		repaint();
 		
+	}
+	public void correctScreen() {
+		title.setText("That is correct good jub slut");
+		repaint();
 	}
 	
 
