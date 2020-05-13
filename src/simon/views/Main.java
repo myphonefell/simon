3a4791769e6d1644c9efab2cc71942ba2aa33346
myphonefell <58866386+myphonefell@users.noreply.github.com>
2020-05-13 15:47:27 -0400

    Update Main.java

--- src/simon/views/Main.java
+++ src/simon/views/Main.java
@@ -11,17 +11,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+
 		View view = new View();
-		SequenceGenerator gen = new SequenceGenerator(view.boxes,1);
-		SequenceListener in = new SequenceListener(view.boxes);
-		
+		SequenceGenerator gen = new SequenceGenerator(1,view);
+		SequenceListener in = new SequenceListener(gen.squares,gen,view);
+
 		frame.setPreferredSize(new Dimension(600,600));
 		frame.setContentPane(view);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 		
-		gen.delay(2000);
+		gen.delay(100);
 		int[] sequence = gen.generateSequence();
 		in.generated = sequence;
 		in.start();
